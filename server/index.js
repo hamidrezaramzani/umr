de32a8f66a0d14f60b39fda6e0f5468d427aae2e
hamidrezaramzani <hamidrezaramzani80@gmail.com>
2023-06-03 20:22:43 +0330

    server module initilize

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const start = () => {
   const app = express();
   const PORT = process.env.PORT || 5000;
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(helmet());
+  app.use(morgan("dev"));
   app.listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.log("Server running at port ", PORT);
