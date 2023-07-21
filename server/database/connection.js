d0dceecc7c33fde69b6403232088bdb243890de7
hamidreza <hamidrezaramzani80@gmail.com>
2023-07-21 18:47:13 +0330

    change mongoose connection method by enviroment

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,13 @@ require("dotenv").config({
   path: path.join(__dirname, "../", ".env"),
 });
 const mongoose = require("mongoose");
-mongoose.connect(encodeURIComponent(process.env.DB_URI)).then((db) => {
+const URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.DEVELOPMENT_DB_URI
+    : `mongodb+srv://hamidrezaramzani:${encodeURIComponent(
+        process.env.DB_PASSWORD
+      )}@cluster0.wmowe.mongodb.net/?retryWrites=true&w=majority;`;
+mongoose.connect(URI).then((db) => {
   // eslint-disable-next-line no-console
   console.log("Database connected to " + db.connection.db.databaseName);
 });
