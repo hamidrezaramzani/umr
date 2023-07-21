1aa4c3248521ee4491296059b1fb8cb56b3dd693
hamidreza <hamidrezaramzani80@gmail.com>
2023-07-21 18:39:15 +0330

    encode mongodb uri string

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ require("dotenv").config({
   path: path.join(__dirname, "../", ".env"),
 });
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_URI).then((db) => {
+mongoose.connect(encodeURIComponent(process.env.DB_URI)).then((db) => {
   // eslint-disable-next-line no-console
   console.log("Database connected to " + db.connection.db.databaseName);
 });
