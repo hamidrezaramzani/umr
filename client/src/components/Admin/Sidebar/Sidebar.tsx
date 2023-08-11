abae834ff967dc83a2935f3bd7ea51af2580c825
hamidreza <hamidrezaramzani80@gmail.com>
2023-08-11 15:30:10 +0330

    feat: change admin sidebar style

--- client/src/components/Admin/Sidebar/Sidebar.tsx
+++ client/src/components/Admin/Sidebar/Sidebar.tsx
@@ -7,14 +7,31 @@ import {
   Box,
   HStack,
   Text,
+  Avatar,
+  Heading,
+  VStack,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { adminSidebarItems } from "../../../constants/AdminSidebar";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const pathSegments = location.pathname.split("/");
+
+  // Get the last segment of the URL
+  const lastSegment = pathSegments[pathSegments.length - 1];
   return (
-    <Box width="20%">
-      <Accordion>
+    <Box width="22%" bg="#fcfcfc" p="3" rounded="md">
+      <HStack width="full">
+        <Avatar />
+        <VStack gap="0" align="right">
+          <Heading fontSize="15">حمیدرضا رمضانی</Heading>
+          <Text fontSize="12" colorScheme="blue">
+            ادمین سایت
+          </Text>
+        </VStack>
+      </HStack>
+      <Accordion allowMultiple defaultIndex={[0, 1, 2]}>
         {adminSidebarItems.map((sidebarItem) => (
           <AccordionItem>
             <h2>
@@ -26,7 +43,13 @@ const Sidebar = () => {
               </AccordionButton>
             </h2>
             {sidebarItem.childrens.map((sidebarChild) => (
-              <AccordionPanel pb={4} color="linkedin.900">
+              <AccordionPanel
+                bg={
+                  sidebarChild.name === lastSegment ? "telegram.100" : undefined
+                }
+                pb={4}
+                color="linkedin.900"
+              >
                 <Link to={sidebarChild.link}>
                   <HStack>
                     <sidebarChild.Icon fontSize={19} />
--- client/src/constants/AdminSidebar.ts
+++ client/src/constants/AdminSidebar.ts
@@ -1,120 +1,134 @@
 import { IconType } from "react-icons";
-import { AiOutlineUserAdd, AiOutlineUser } from "react-icons/ai"
+import {
+  AiOutlineUserAdd,
+  AiOutlineUser,
+  AiFillDashboard,
+} from "react-icons/ai";
 import { MdOutlineFastfood } from "react-icons/md";
 import { BiFoodMenu } from "react-icons/bi";
 import { RiReservedLine, RiUser2Fill } from "react-icons/ri";
 export interface AdminSidebarProps {
+  title: string;
+  childrens: {
     title: string;
-    childrens: {
-        title: string;
-        link: string;
-        name: string;
-        Icon: IconType
-    }[]
+    link: string;
+    name: string;
+    Icon: IconType;
+  }[];
 }
 
 export const adminSidebarItems: AdminSidebarProps[] = [
-    {
-        title: "دانشجو",
-        childrens: [
-            {
-                title: "اضافه کردن دانشجو",
-                link: "/admin/add-student",
-                name: "add-student",
-                Icon: AiOutlineUserAdd
-            },
-            {
-                title: "مدیریت دانشجو",
-                link: "/admin/manage-student",
-                name: "manage-student",
-                Icon: AiOutlineUser
-            },
-        ]
-    },
-    {
-        title: "غذا",
-        childrens: [
-            {
-                title: "اضافه کردن غذای جدید",
-                link: "/admin/add-meal",
-                name: "add-meal",
-                Icon: MdOutlineFastfood,
-            },
-            {
-                title: "مدیریت غذاها",
-                link: "/admin/manage-meals",
-                name: "manage-meals",
-                Icon: MdOutlineFastfood
-            },
-        ]
-    },
-    {
-        title: "غذای اضافه",
-        childrens: [
-            {
-                title: "اضافه کردن غذای اضافه جدید",
-                link: "/admin/add-extra-meal",
-                name: "add-extra-meal",
-                Icon: MdOutlineFastfood,
-            },
-            {
-                title: "مدیریت غذاهای اضافه",
-                link: "/admin/manage-extra-meals",
-                name: "manage-extra-meals",
-                Icon: MdOutlineFastfood
-            },
-        ]
-    },
-    {
-        title: "وقت غذاها",
-        childrens: [
-            {
-                title: "اضافه کردن وقت غذا",
-                link: "/admin/add-mealTime",
-                name: "add-mealTime",
-                Icon: BiFoodMenu
-            },
-            {
-                title: "مدیریت وقت غذاها",
-                link: "/admin/manage-mealTimes",
-                name: "manage-mealTimes",
-                Icon: BiFoodMenu
-            },
-        ]
-    },
-    {
-        title: "منو",
-        childrens: [
-            {
-                title: "اضافه کردن آیتم منو",
-                link: "/admin/add-menu",
-                name: "add-menu",
-                Icon: RiReservedLine
-            },
-            {
-                title: "مشاهده منو",
-                link: "/admin/manage-menu",
-                name: "manage-menu",
-                Icon: RiReservedLine
-            },
-        ]
-    },
-    {
-        title: "حساب",
-        childrens: [
-            {
-                title: "تنظیمات",
-                link: "/admin/settings",
-                name: "settings",
-                Icon: RiUser2Fill
-            },
-            {
-                title: "خروج",
-                link: "/logout",
-                name: "manage-menu",
-                Icon: RiUser2Fill
-            },
-        ]
-    },
-]
-
+  {
+    title: "خانه",
+    childrens: [
+      {
+        title: "داشبورد",
+        link: "/admin/dashboard",
+        name: "dashboard",
+        Icon: AiFillDashboard,
+      },
+    ],
+  },
+  {
+    title: "دانشجو",
+    childrens: [
+      {
+        title: "اضافه کردن دانشجو",
+        link: "/admin/add-student",
+        name: "add-student",
+        Icon: AiOutlineUserAdd,
+      },
+      {
+        title: "مدیریت دانشجو",
+        link: "/admin/manage-student",
+        name: "manage-student",
+        Icon: AiOutlineUser,
+      },
+    ],
+  },
+  {
+    title: "غذا",
+    childrens: [
+      {
+        title: "اضافه کردن غذای جدید",
+        link: "/admin/add-meal",
+        name: "add-meal",
+        Icon: MdOutlineFastfood,
+      },
+      {
+        title: "مدیریت غذاها",
+        link: "/admin/manage-meals",
+        name: "manage-meals",
+        Icon: MdOutlineFastfood,
+      },
+    ],
+  },
+  {
+    title: "غذای اضافه",
+    childrens: [
+      {
+        title: "اضافه کردن غذای اضافه جدید",
+        link: "/admin/add-extra-meal",
+        name: "add-extra-meal",
+        Icon: MdOutlineFastfood,
+      },
+      {
+        title: "مدیریت غذاهای اضافه",
+        link: "/admin/manage-extra-meals",
+        name: "manage-extra-meals",
+        Icon: MdOutlineFastfood,
+      },
+    ],
+  },
+  {
+    title: "وقت غذاها",
+    childrens: [
+      {
+        title: "اضافه کردن وقت غذا",
+        link: "/admin/add-mealTime",
+        name: "add-mealTime",
+        Icon: BiFoodMenu,
+      },
+      {
+        title: "مدیریت وقت غذاها",
+        link: "/admin/manage-mealTimes",
+        name: "manage-mealTimes",
+        Icon: BiFoodMenu,
+      },
+    ],
+  },
+  {
+    title: "منو",
+    childrens: [
+      {
+        title: "اضافه کردن آیتم منو",
+        link: "/admin/add-menu",
+        name: "add-menu",
+        Icon: RiReservedLine,
+      },
+      {
+        title: "مشاهده منو",
+        link: "/admin/manage-menu",
+        name: "manage-menu",
+        Icon: RiReservedLine,
+      },
+    ],
+  },
+  {
+    title: "حساب",
+    childrens: [
+      {
+        title: "تنظیمات",
+        link: "/admin/settings",
+        name: "settings",
+        Icon: RiUser2Fill,
+      },
+      {
+        title: "خروج",
+        link: "/logout",
+        name: "manage-menu",
+        Icon: RiUser2Fill,
+      },
+    ],
+  },
+];
