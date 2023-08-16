54108e94cc130bbaec53ddb71ae225bfbc27c699
hamidreza <hamidrezaramzani80@gmail.com>
2023-08-16 16:21:15 +0330

    feat: wip

--- client/src/pages/Admin/Dashboard/Dashboard.tsx
+++ client/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,23 +1,11 @@
-import { HStack } from "@chakra-ui/react";
+import { Text, Heading } from "@chakra-ui/react";
 import AdminDashboardContainer from "../../../components/Admin/AdminDashboardContainer/AdminDashboardContainer";
-import DashboardDetail from "../../../components/Admin/DashboardDetail/DashboardDetail";
 
 const Dashboard = () => {
   return (
     <AdminDashboardContainer title="داشبورد">
-      <HStack wrap="wrap">
-        <DashboardDetail width="49%" title="لیست آخرین دانشجوها">
-          Hello
-        </DashboardDetail>
-
-        <DashboardDetail width="49%" title="پر امتیاز ترین غذاهای سایت">
-          Hello
-        </DashboardDetail>
-
-        <DashboardDetail width="100%" title="نمودار هفتگی رزرو غذا">
-          Hello
-        </DashboardDetail>
-      </HStack>
+      <Heading>خوش آمدید</Heading>
+      <Text>به پنل ادمین خوش آمدید</Text>
     </AdminDashboardContainer>
   );
 };
